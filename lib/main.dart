31071bfb133fc4bd883dfa0fffb67c9be3d8d21b
dev3bitwaytech <dev3@bitwaytech.com>
2023-03-24 12:51:24 +0530

    drawer1.dart

--- lib/main.dart
+++ lib/main.dart
@@ -11,13 +11,11 @@ void main() async{
     PhoneAuthProvider(),
   ]);
 
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
