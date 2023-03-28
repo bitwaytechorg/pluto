85dcd6e0b43f11bc5fa96bc613e9cbdb42246cac
dev3bitwaytech <dev3@bitwaytech.com>
2023-03-28 15:11:42 +0530

    information

---
+++ lib/screens/tablet/tablet_information.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class Tablet_Information extends StatefulWidget{
+
+  @override
+  Tablet_InformationState createState()=> Tablet_InformationState();
+}
+
+class Tablet_InformationState extends State<Tablet_Information> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
