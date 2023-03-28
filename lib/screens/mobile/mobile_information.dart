c634887c172cb824f9af3228d06118cf5747412e
dev3bitwaytech <dev3@bitwaytech.com>
2023-03-28 15:06:24 +0530

    information

---
+++ lib/screens/mobile/mobile_information.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class Mobile_Information extends StatefulWidget{
+
+  @override
+  Mobile_InformationState createState()=> Mobile_InformationState();
+}
+
+class Mobile_InformationState extends State<Mobile_Information>{
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        child: Text("Bhumika"),
+      ),
+    );
+  }
+
+}
