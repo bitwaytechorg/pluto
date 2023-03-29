666e6effd4ecfa765665799e6b5ec5962f3d8fee
dev3bitwaytech <dev3@bitwaytech.com>
2023-03-29 14:22:09 +0530

    mobile_user_profile

--- lib/screens/mobile/mobile_user_profile.dart
+++ lib/screens/mobile/mobile_user_profile.dart
@@ -2,6 +2,11 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:pluto/components/avatar.dart';
 
+import '../../components/notification_alert.dart';
+import '../../components/scroll_behaviour.dart';
+import '../../components/slider_menu.dart';
+import '../../components/topbar.dart';
+
 class Mobile_UserProfile extends StatefulWidget{
 
   @override
@@ -9,27 +14,82 @@ class Mobile_UserProfile extends StatefulWidget{
 }
 
 class Mobile_UserProfileState extends State<Mobile_UserProfile> {
+  double xOffset = 0;
+  double yOffset = 0;
+  double scalefactor = 1;
+  bool isDrawerOpen = false;
+
+  void toggleMenu() {
+    bool tmpStatus = !isDrawerOpen;
+    setState(() {
+      xOffset = tmpStatus ? 250 : 0;
+      yOffset = tmpStatus ? 50 : 0;
+      scalefactor = tmpStatus ? 0.91 : 1;
+      isDrawerOpen = tmpStatus;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
         top: true,
-        child: Container(
-          child: Column(
-              children: [
-                Row(
-                    children: [
-                      Padding(
-                        padding: const EdgeInsets.all(10),
-                        child: Avatar(
-                            size: 80, ImageURL: ' ',),
-                      )
-                    ]
+        child: Stack(
+          children: [
+            Slider_menu(),
+            AnimatedContainer(
+              width: MediaQuery.of(context).size.width,
+              transform: Matrix4.translationValues(xOffset, yOffset, 0)
+                ..scale(scalefactor)
+                ..rotateY(isDrawerOpen ? -0.5 : 0),
+              duration: Duration(milliseconds: 250),
+              decoration: BoxDecoration(
+                  color: Theme.of(context).colorScheme.surface,
+                  borderRadius: BorderRadius.circular(isDrawerOpen ? 40 : 0.0)),
+              child: Column(children: [
+                TopBar(
+                  isDrawerOpen: isDrawerOpen,
+                  onTap: toggleMenu,
+                  background: Colors.transparent,
                 ),
-              ]
-          ),
+                Expanded(
+                  child: ScrollConfiguration(
+                    behavior: MyBehavior(),
+                    child: SingleChildScrollView(
+                      child: buildContent(),
+                    ),
+                  ),
+                ),
+
+              ]),
+            ),
+          ],
         ),
       ),
+    );
+  }
+
+  buildContent() {
+    return Container(
+          child: Column(
+              children: [
+                Container(
+                  child: Row(
+                      children: [
+                        Padding(
+                          padding: EdgeInsets.only(top: 30, left: 30),
+                          child: Avatar(
+                              size: 100, ImageURL: ' ',),
+                        )
+                      ]
+                  ),
+                ),
+                Container(
+
+                )
+              ]
+
+          ),
     );
   }
 }
