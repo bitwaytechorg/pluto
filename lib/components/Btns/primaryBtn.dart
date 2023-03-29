8ff052c68ead49e81c3f45fa40b62a7266ac243d
Dileep <dev2@bitwaytech.com>
2023-03-29 11:22:44 +0530

    public profile .

--- lib/components/Btns/primaryBtn.dart
+++ lib/components/Btns/primaryBtn.dart
@@ -1,21 +1,28 @@
 import 'package:flutter/material.dart';
+import 'package:pluto/config/config.dart' as CONFIG;
 
 class PrimaryBtn extends StatelessWidget {
   String ButtonTitle;
-  PrimaryBtn({Key? key, required this.ButtonTitle});
+  double width;
+  double height;
+  PrimaryBtn({Key? key, required this.ButtonTitle, this.width=200, this.height=50});
 
   @override
   Widget build(BuildContext context) {
     return Container(
+      height: height,
+      width: width,
       decoration: BoxDecoration(
         borderRadius: BorderRadius.circular(40),
-        color: Colors.blue,
+        color: CONFIG.primaryColor,
       ),
       child: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 50, vertical: 10),
-        child: Text(ButtonTitle, style: TextStyle(
-          color: Colors.white,
-        ),),
+        padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+        child: Center(
+          child: Text(ButtonTitle, style: TextStyle(
+            color: Colors.white,
+          ),),
+        ),
       ),
     );
   }
