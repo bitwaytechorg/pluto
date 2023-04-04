4e5a104357779a45e338f6f471fa1e1f7ddd1be6
dev3bitwaytech <dev3@bitwaytech.com>
2023-04-04 16:57:52 +0530

    notification.dart

--- lib/components/notification.dart
+++ lib/components/notification.dart
@@ -1,15 +1,44 @@
-import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
-class Notification extends StatefulWidget {
+import 'avatar.dart';
+import 'notification.dart';
 
+class NotificationItems extends StatefulWidget {
+  String notification;
+  String time;
+   NotificationItems({Key? key, required this.notification, required this.time,}) : super(key: key);
 
   @override
-  State<Notification> createState() => _NotificationState();
+  State<NotificationItems> createState() => _NotificationItemsState();
 }
 
-class _NotificationState extends State<Notification> {
+class _NotificationItemsState extends State<NotificationItems> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Container(child: Column(
+        children: [
+          Container(
+            height: 50,
+            child: Row(
+              children: [
+                Padding(
+                  padding:EdgeInsets.only(top: 5, left: 15),
+                  child: Avatar(size: 40, ImageURL: ""),
+                ),
+                Padding(
+                    padding: EdgeInsets.only(left: 15, ),
+                    child: Text( widget.notification)),
+                Padding(
+                  padding: const EdgeInsets.only(left: 50),
+                  child: Text(widget.time),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),),
+    );
   }
 }
--- lib/screens/notification.dart
+++ lib/screens/notification.dart
@@ -1,10 +1,8 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:font_awesome_flutter/font_awesome_flutter.dart';
-import '../components/Btns/primaryBtn.dart';
 import '../components/avatar.dart';
 import '../components/scroll_behaviour.dart';
-import '../components/search_bar.dart';
+import 'package:pluto/components/notification.dart';
 import 'home.dart';
 import 'package:pluto/config/config.dart' as CONFIG;
 
@@ -14,26 +12,58 @@ class NotificationPage extends StatefulWidget {
 }
 
 class _NotificationPageState extends State<NotificationPage> {
-  // List notifications = [
-  //   "It is used to develop applications,A beautiful, extreme smooth and quite nice shoping app interface UI by using Dart + Flutter.It is used to develop applications.A beautiful, extreme smooth and quite nice shoping app interface UI by using Dart + Flutter.",
-  //   "method of creating applications.A beautiful, extreme smooth and quite nice shoping app interface UI by using Dart + Flutter.It is used to develop applications.A beautiful, extreme smooth and quite nice shoping app interface UI by using Dart + Flutter.",
-  //   "A beautiful, extreme smooth and quite nice shoping app interface UI by using Dart + Flutter.It is used to develop applications.A beautiful, extreme smooth and quite nice shoping app interface UI by using Dart + Flutter.",
-  //   "It is used to develop applications.A beautiful, extreme smooth and quite nice shoping app interface UI by using Dart + Flutter. It is used to develop applications.A beautiful, extreme smooth and quite nice shoping app interface UI by using Dart + Flutter.",
-  //   "method of creating applications.A beautiful, extreme smooth and quite nice shoping app interface UI by using Dart + Flutter.It is used to develop applications.A beautiful, extreme smooth and quite nice shoping app interface UI by using Dart + Flutter.",
-  //   "It is used to develop applications.A beautiful, extreme smooth and quite nice shoping app interface UI by using Dart + Flutter. ",
-  //   "method of creating applications.A beautiful, extreme smooth and quite nice shoping app interface UI by using Dart + Flutter.",
-  //   "It is used to develop applications.A beautiful, extreme smooth and quite nice shoping app interface UI by using Dart + Flutter.",
-  //   "method of creating applications.A beautiful, extreme smooth and quite nice shoping app interface UI by using Dart + Flutter.It is used to develop applications.A beautiful, extreme smooth and quite nice shoping app interface UI by using Dart + Flutter.",
-  //   "It is used to develop applications.A beautiful, extreme smooth and quite nice shoping app interface UI by using Dart + Flutter.It is used to develop applications.A beautiful, extreme smooth and quite nice shoping app interface UI by using Dart + Flutter. ",
-  //
-  // ];
+  List today = [
+    "new massage",
+    "It is used to develop applications",
+    "It is used to develop applications",
+    "It is used to develop applications",
+    "It is used to develop applications",
+  ];
+
+  List yesterday = [
+    "new massage",
+    "It is used to develop applications",
+    "It is used to develop applications",
+    "It is used to develop applications",
+    "It is used to develop applications",
+  ];
+
+  List thisMonth = [
+    "new massage",
+    "It is used to develop applications",
+    "It is used to develop applications",
+    "It is used to develop applications",
+    "It is used to develop applications",
+  ];
+
+  List Notification = [
+    "It is used to develop applications",
+    "It is used to develop applications",
+    "It is used to develop applications",
+    "It is used to develop applications",
+    "It is used to develop applications",
+    "It is used to develop applications",
+    "It is used to develop applications",
+    "It is used to develop applications",
+    "It is used to develop applications",
+    "It is used to develop applications",
+    "It is used to develop applications",
+  ];
+  List time = [
+    "1 hr",
+    "3 hr",
+    "5 hr",
+    "10 hr",
+    "20 hr",
+    "24 hr",
+  ];
+
   List days = [
     "Today",
     "This week",
     "This month",
     "Earlier",
   ];
-
 
   @override
   Widget build(BuildContext context) {
@@ -59,12 +89,12 @@ class _NotificationPageState extends State<NotificationPage> {
                                         builder: (context) => Home()));
                               },
                               child:
-                              Icon(Icons.arrow_back, color: Colors.grey)),
+                                  Icon(Icons.arrow_back, color: Colors.grey)),
                           SizedBox(
                             width: 15,
                           ),
                           Text(
-                            "Notifications",
+                            "NotificationPages",
                             style: TextStyle(
                               fontSize: 20,
                               color: Colors.black,
@@ -74,36 +104,42 @@ class _NotificationPageState extends State<NotificationPage> {
                         ],
                       ),
                       Padding(
-                        padding: const EdgeInsets.symmetric(horizontal: 10,),
+                        padding: const EdgeInsets.symmetric(
+                          horizontal: 10,
+                        ),
                         child: Row(
                           children: [
-                            SizedBox(width: 25,),
+                            SizedBox(
+                              width: 25,
+                            ),
                             Container(
                                 height: 35,
                                 width: 35,
                                 decoration: BoxDecoration(
-                                  borderRadius: BorderRadius.all(
-                                      Radius.circular(40)),
-                                  border: Border.all(width: 1,
-                                      color: CONFIG.primaryColor.withAlpha(
-                                          100)),
-
+                                  borderRadius:
+                                      BorderRadius.all(Radius.circular(40)),
+                                  border: Border.all(
+                                      width: 1,
+                                      color:
+                                          CONFIG.primaryColor.withAlpha(100)),
                                 ),
                                 child: InkWell(
                                     onTap: () => {},
-                                    child: Icon(Icons.more_vert_sharp,
-                                      color: CONFIG.primaryColor, size: 25,))
-                            )
+                                    child: Icon(
+                                      Icons.more_vert_sharp,
+                                      color: CONFIG.primaryColor,
+                                      size: 25,
+                                    )))
                           ],
                         ),
                       ),
-
                     ]),
               ),
-
               Divider(color: Colors.grey),
               Expanded(
-                  child: buildContent()),
+                child: SingleChildScrollView(
+                    physics: ScrollPhysics(), child: buildContent()),
+              )
             ],
           ),
         ),
@@ -112,104 +148,96 @@ class _NotificationPageState extends State<NotificationPage> {
   }
 
   buildContent() {
-    return Container(
-      child: Column(
-        children: [
-          Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Flexible(
-                  child: Container(
-                    padding: const EdgeInsets.all(10),
-                    child: Avatar(size: 80,
-                        ImageURL: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcReSQp9MqQWLBsNJQ0C9OA4uSKKkqlI6-hUog&usqp=CAU"),
-                  ),
-                ),
-                Container(
-                  child: Column(
-                    children: [
-                      Padding(
-                        padding: const EdgeInsets.only(right: 200,bottom: 5),
-                        child: Text("Follow Request", style: TextStyle(fontWeight: FontWeight.bold),),
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.only(right: 145,),
-                        child: Text("Approve or ignore request", style: TextStyle(fontSize: 13),),
-                      ),
-
-                    ],
-                  ),
-                ),
-
-
-
-                // BuildContent() {
-                // return ListView.builder(
-                //      shrinkWrap: true,
-                //      scrollDirection:Axis.vertical,
-                //      itemCount: notifications.length, itemBuilder: (BuildContext context, int index) {
-                //        return Padding(
-                //          padding: const EdgeInsets.only(top: 20, left: 10,),
-                //            child: Container(
-                //              width: 125,
-                //              decoration: BoxDecoration(
-                //                borderRadius: BorderRadius.circular(5),
-                //                color: Colors.grey[100],
-                //              ),
-                //                  child: Column(
-                //                    children: [
-                //                      Column(
-                //                        children: [
-                //                          Padding(
-                //                            padding: EdgeInsets.only(top: 5, left: 300,),
-                //                            child: Text("2 Apr",),
-                //                          ),
-                //                          Row(
-                //                            children:[
-                //                             Avatar(size: 65, ImageURL: '',),
-                //                               Column(
-                //                                 children: [
-                //                                   Text("User Name" , style: TextStyle(fontSize: 20, ),
-                //                                   ),
-                //                                   SizedBox(height: 10,),
-                //                                   Padding(
-                //                                     padding: const EdgeInsets.only(left: 20,),
-                //                                     child: Container(
-                //                                         width: MediaQuery.of(context).size.width-110,
-                //                                         child: Text(notifications[index], style: TextStyle(fontSize: 13),)),
-                //                                   )
-                //                                 ],
-                //                               )],
-                //                          ),
-                //                        ],
-                //                      ),
-                //                      Padding(
-                //                        padding: const EdgeInsets.only(bottom: 10, left: 10, top: 25),
-                //                        child: Row(
-                //                          children: [
-                //                            Text(" Marked as read", style: TextStyle(fontSize: 15),),
-                //                            Padding(
-                //                              padding: const EdgeInsets.only(left: 240),
-                //                              child: Icon(FontAwesomeIcons.trashCan, size: 20,),
-                //                            ),
-                //
-                //                          ],
-                //                        ),
-                //                      )
-                //
-                //                    ],
-                //
-                //                  ),
-                //                ),
-                //
-                //            );
-                //      });
-                //
-              ]
+    return Column(
+      children: [
+        Row(mainAxisAlignment: MainAxisAlignment.spaceBetween, children: [
+          Flexible(
+            child: Container(
+              padding: const EdgeInsets.all(10),
+              child: Avatar(
+                  size: 80,
+                  ImageURL:
+                      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcReSQp9MqQWLBsNJQ0C9OA4uSKKkqlI6-hUog&usqp=CAU"),
+            ),
           ),
-        ],
-      ),
+          Container(
+            child: Column(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(right: 200, bottom: 5),
+                  child: Text(
+                    "Follow Request",
+                    style: TextStyle(fontWeight: FontWeight.bold),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(
+                    right: 145,
+                  ),
+                  child: Text(
+                    "Approve or ignore request",
+                    style: TextStyle(fontSize: 13),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ]),
+        SizedBox(
+          height: 30,
+        ),
+        Padding(
+          padding: const EdgeInsets.only(left: 20, top: 5, bottom: 5),
+          child: Align(
+              alignment: Alignment.centerLeft,
+              child: Text(
+                "Today",
+                style: TextStyle(fontWeight: FontWeight.bold),
+              )),
+        ),
+        Container(
+          child: ListView.builder(
+              shrinkWrap: true,
+              itemCount: today.length,
+              itemBuilder: (BuildContext context, int index) {
+                return Container(
+                  child: NotificationItems(
+                      notification: Notification[index], time: time[index]),
+                );
+              }),
+        ),
+        Padding(
+          padding: const EdgeInsets.only(left: 20, top: 5, bottom: 5),
+          child: Align(alignment: Alignment.centerLeft, child: Text("Yesterday", style: TextStyle(fontWeight: FontWeight.bold))),
+        ),
+        ListView.builder(
+          shrinkWrap: true,
+          itemCount: yesterday.length,
+          itemBuilder: (BuildContext context, int index) {
+            return Container(
+              child: NotificationItems(
+                notification: Notification[index],
+                time: time[index],
+              ),
+            );
+          },
+        ),
+        Padding(
+          padding: const EdgeInsets.only(left: 20, top: 5, bottom: 5),
+          child: Align(alignment: Alignment.centerLeft, child: Text("This Month",style: TextStyle(fontWeight: FontWeight.bold))),
+        ),
+        ListView.builder(
+            shrinkWrap: true,
+            itemCount: thisMonth.length,
+            itemBuilder: (BuildContext context, int index) {
+              return Container(
+                child: NotificationItems(
+                  notification: Notification[index],
+                  time: time[index],
+                ),
+              );
+            })
+      ],
     );
-
   }
 }
