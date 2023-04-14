63e44850f8feccaf8b15b0bfe53f196a5e65c18f
Dileep <dev2@bitwaytech.com>
2023-04-14 10:41:03 +0530

    product list tile updated.

--- lib/components/product_list_tile.dart
+++ lib/components/product_list_tile.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
-
-import 'avatar.dart';
+import 'cText.dart';
 
 class ProductListTile extends StatefulWidget {
   String itemPic;
@@ -20,24 +19,27 @@ class _ProductListTileState extends State<ProductListTile> {
       height: 100,
       padding: EdgeInsets.only(left: 10, right: 20),
       decoration: BoxDecoration(
-        border: Border(bottom: BorderSide(width: 0.3))
+        border: Border(bottom: BorderSide(width: 0.2))
       ),
       child: Row(children: [
         Image.network(widget.itemPic,
           height: 85,
           width: 90,
         ),
-        SizedBox(width: 10,),
+        SizedBox(width: 20,),
         Column(children: [
-          SizedBox(height: 30,),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-            Text(widget.productName, style: TextStyle(fontSize: 18),),
-            Text("Rs 599"),
-          ],),
-          Text(widget.productDescription, style: TextStyle(
-              fontSize: 15, color: Colors.grey),),
+          SizedBox(height: 15,),
+          Container(
+            width: MediaQuery.of(context).size.width-145,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+              Container( width: 160,child: CText(text:widget.productName, fontSize: 17,)),
+              CText(text:"Rs. ${widget.productPrice.toString()}", color: Colors.grey, fontSize: 13,),
+            ],),
+          ),
+          SizedBox(height: 10,),
+          Container(width:MediaQuery.of(context).size.width-145 , child: CText(text:widget.productDescription, fontSize: 14, maxLines: 2,)),
         ],),
       ],),
     );
